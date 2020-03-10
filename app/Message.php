c975db0d9aa4feb047c6237e58833b478361da83
ngovietan285 <ngovietan285@gmail.com>
2020-03-10 17:11:02 +0700

    fix database2

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    public function group()
+    {
+        return $this->belongsTo('App\Group');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
